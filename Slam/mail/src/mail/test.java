c7924a6a546d8e7d4c1f391fbc907ce3f6fa6172
Sanfujinkai <hssaini.saad@hotmail.fr>
2014-12-08 14:31:02 +0100

    saad partie mail

---
+++ Slam/mail/src/mail/test.java
@@ -0,0 +1,25 @@
+package mail;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+
+
+
+public class test {
+	
+	public static void main(String[] args) throws AddressException, MessagingException {
+	String texte ="TEST MAIL BTS SIO";
+	String adresse="acerdove@hotmail.fr";
+
+	
+	EnvoiMail.envoiMail(adresse, texte); 
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+}
