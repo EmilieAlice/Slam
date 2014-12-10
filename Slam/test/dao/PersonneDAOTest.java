9bdb7ed885017912a689421c1c3439e4f189601e
lebaronjerome <lebaronjerome@free.fr>
2014-12-10 10:33:50 +0100

    Tout va bien !!!!

--- Slam/test/dao/PersonneDAOTest.java
+++ Slam/test/dao/PersonneDAOTest.java
@@ -16,7 +16,7 @@ public class PersonneDAOTest {
 	
 	@Test
 	public void testFindById() throws SQLException {
-		Timestamp time = Timestamp.valueOf("2014-12-08 13:06:37");
+		Timestamp time = Timestamp.valueOf("2014-12-10 09:13:28");
 
 		Personne expected = new Personne(2, "Mle", "Emilie", "WAILLE", "25 Avenue de la gare", "92000", "NANTERRE",
 				"0956789101", "0321033738", "waille@hotmail.fr", "walle", time, false);
