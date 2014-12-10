1e8f1c9c6e6ff3ea38697b3c34609884fe59a066
jerdopler <lebaronjerome@free.fr>
2014-12-10 09:30:03 +0100

    Essai pour vérifier si le git ignore a fonctionné

--- Slam/test/modele/Main.java
+++ Slam/test/modele/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Emilie");
 		System.out.println("Un petit commit de Jérome");
+		System.out.println("Test du git ignore");
 
 	}
 
