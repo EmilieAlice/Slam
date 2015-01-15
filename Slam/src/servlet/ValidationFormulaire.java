6291277739359c9c93e96c6753afc2d25b72eb6b
EmilieAlice <ewaille@hotmail.fr>
2015-01-15 19:26:12 +0100

    Reindentation

--- Slam/src/servlet/ValidationFormulaire.java
+++ Slam/src/servlet/ValidationFormulaire.java
@@ -10,14 +10,14 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ValidationFormulaire extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ValidationFormulaire() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ValidationFormulaire() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
