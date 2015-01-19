2f40274002e7530df1d684d2eee3ab066644f15d
Hazerfox <hazerfox@live.fr>
2015-01-19 15:29:58 +0100

    Mise a jour

--- Slam/src/dao/DataBase.java
+++ Slam/src/dao/DataBase.java
@@ -1,21 +1,24 @@
 package dao;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 
 public class DataBase {
 
 
 	protected static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	protected static final String URL = "jdbc:mysql://localhost/lagarenne2015";
-	//protected static final String URL = "jdbc:mysql://192.168.92.10/lagarenne2015";
+	protected static final String URL = "jdbc:mysql://localhost:3307/lagarenne2015";
+	//protected static final String sURL = "jdbc:mysql://192.168.92.10/lagarenne2015";
 	protected static final String USER = "lagarenne2015";
 	protected static final String PASSWORD = "lagarenne2015";
 
 	public enum SortOrder {
 		ASC, DESC;
-	}
-
-
+	}	
+	
 	static {
 		// Chargement du pilote
 		// Ne doit avoir lieu qu'une seule fois
