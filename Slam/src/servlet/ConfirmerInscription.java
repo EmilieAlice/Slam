b7ebe45c405243f7a3a5eb6e600934ebe97c6da5
Michel Plasse <mplasse@free.fr>
2015-01-28 12:11:09 +0100

    Corrige le nom du serveur, son port et le contexte dans Inscrire et ConfirmerInscription. Utilise request.getServerName, getServerPort et getContextPath pour cela. Il faut que dans votre projet, le contexte soit /Slam, et non pas vide ou autre chose

--- Slam/src/servlet/ConfirmerInscription.java
+++ Slam/src/servlet/ConfirmerInscription.java
@@ -14,80 +14,85 @@ import mail.EnvoiMail;
 import modele.Personne;
 
 /**
- * Confirmer l'inscription d'un candidat suite à un mail de validation.
+ * Confirmer l'inscription d'un candidat suite à un mail de
+ * validation.
  */
 public class ConfirmerInscription extends HttpServlet {
 
-    /** URL du serveur où rediriger */
-//    private static final String SERVER_URL = "http://localhost:8080";
-    private static final String SERVER_URL = "http://http://sio92250.no-ip.org:18080/Slam";
- 
-    private Personne personne;
+  private Personne personne;
 
-    /**
-     * Confirmer l'inscription du candidat. Avertit l'utilisateur dans
-     * l'attribut msg (ok ou erreur).
-     *
-     * @param requestw
-     * @param response
-     * @throws ServletException
-     * @throws IOException
-     */
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        String cle = request.getParameter("cle");
-        String email = request.getParameter("email");
-        String msg = null;
+  /**
+   * Confirmer l'inscription du candidat. Avertit l'utilisateur dans
+   * l'attribut msg (ok ou erreur).
+   *
+   * @param requestw
+   * @param response
+   * @throws ServletException
+   * @throws IOException
+   */
+  protected void doGet(HttpServletRequest request,
+      HttpServletResponse response) throws ServletException, IOException {
+    String cle = request.getParameter("cle");
+    String email = request.getParameter("email");
+    String msg = null;
 
-        long ms = Long.parseLong(cle);
-        Timestamp time = new Timestamp(ms);
+    long ms = Long.parseLong(cle);
+    Timestamp time = new Timestamp(ms);
+    try {
+      PersonneHome dao = new PersonneHome();
+      boolean ok = dao.valider(email, time);
+
+      if (ok) {
+
+        //----------------------------------------------------
+        String vue = null;
         try {
-            PersonneHome dao = new PersonneHome();
-            boolean ok = dao.valider(email, time);
+          assert personne.getCivilite() != null;
+          msg = "Votre inscription est validée. Vous pouvez accéder à votre "
+              + "<a href='http://" + request.getServerName()
+              + ":" + request.getServerPort()
+              + request.getContextPath()
+              + "/EspacePersonnel?email="
+              + email
+              + "'>espace personnel</a>";
+          EnvoiMail.envoyer(
+              email,
+              "Votre inscription sur Agriotes est validée",
+              msg);
 
-            if (ok) {
-
-                //----------------------------------------------------
-                String vue = null;
-                try {
-                    assert personne.getCivilite() != null;
-                    msg = "Votre inscription est validée. Vous pouvez accéder à votre "
-                            + "<a href='" + SERVER_URL + "/EspacePersonnel?email="
-                            + email
-                            + "'>espace personnel</a>";
-                    EnvoiMail.envoyer(
-                            email,
-                            "Votre inscription sur Agriotes est validée",
-                            msg);
-
-                } catch (AddressException e) {
-                    e.printStackTrace();
-                    msg = e.getMessage();
-                } catch (MessagingException e) {
-                    e.printStackTrace();
-                    msg = e.getMessage();
-                }
-                request.setAttribute("msg", msg);
-                vue = "/WEB-INF/message.jsp";
-            } else {
-                msg = "Le lien d'inscription a expiré. Veuillez vous réinscrire";
-            }
-        } catch (NumberFormatException exc) {
-            msg = "La clé indiquée est invalide. Veuillez vous réinscrire";
-        } catch (SQLException exc) {
-            msg = "Problème de connexion à la base de données. Veuillez"
-                    + " réessayer plus tard." + exc.getMessage();
         }
-
+        catch (AddressException e) {
+          e.printStackTrace();
+          msg = e.getMessage();
+        }
+        catch (MessagingException e) {
+          e.printStackTrace();
+          msg = e.getMessage();
+        }
         request.setAttribute("msg", msg);
-        String vue = "/WEB-INF/message.jsp";
-        request.getRequestDispatcher(vue).forward(request, response);
+        vue = "/WEB-INF/message.jsp";
+      }
+      else {
+        msg = "Le lien d'inscription a expiré. Veuillez vous réinscrire";
+      }
+    }
+    catch (NumberFormatException exc) {
+      msg = "La clé indiquée est invalide. Veuillez vous réinscrire";
+    }
+    catch (SQLException exc) {
+      msg = "Problème de connexion à la base de données. Veuillez"
+          + " réessayer plus tard." + exc.getMessage();
     }
 
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
-        throw new UnsupportedOperationException();
+    request.setAttribute("msg", msg);
+    String vue = "/WEB-INF/message.jsp";
+    request.getRequestDispatcher(vue).forward(request, response);
+  }
 
-    }
+  protected void doPost(HttpServletRequest request,
+      HttpServletResponse response) throws ServletException, IOException {
+    throw new UnsupportedOperationException();
+
+  }
 
 }
