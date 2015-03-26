40254f8df107f9705d3440577e3ad29c0b5fa184
jamallasri <jamallasri@outlook.com>
2015-03-26 16:03:02 +0100

    la session Agriotes

---
+++ Slam/src/contexte/SessionAgriotes.java
@@ -0,0 +1,17 @@
+package contexte;
+
+import modele.Personne;
+
+public class SessionAgriotes {
+	private Personne user;
+
+	public Personne getUser() {
+		return user;
+	}
+
+	public void setUser(Personne user) {
+		this.user = user;
+	}
+	
+
+}
